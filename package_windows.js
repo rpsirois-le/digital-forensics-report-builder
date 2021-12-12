1bae0e0e8c85e7498ab341750b94bf1e86444d52
Robert Sirois <rpsirois@gmail.com>
2021-12-12 08:26:29 -0700

    Update Windows builder

--- package_windows.js
+++ package_windows.js
@@ -3,7 +3,7 @@ const electronInstaller = require( 'electron-winstaller' )
 async function packageForWindows() {
     try {
         await electronInstaller.createWindowsInstaller({
-            appDirectory: '.'
+            appDirectory: './Digital Forensics Report Builder-win32-x64'
             , outputDirectory: './build/windows'
             , iconUrl: './ico.ico'
         })
